b1a729aaf6db0ac132d60daf47e58d739d2166d9
Frans Skarman <frans.skarman@gmail.com>
2017-02-27 17:29:41 +0100

    Work on moving things around

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var lastRenderTime = 0;
 // Currently active VRDisplay.
 var vrDisplay;
 // How big of a box to render.
-var boxSize = 1000;
+var boxSize = 25;
 // Various global THREE.Objects.
 var scene;
 var cube;
@@ -25,7 +25,15 @@ var floor;
 var floorMaterial;
 
 var testCube;
+var couch;
 
+var cursor;
+
+var LOOK_AROUND = 0;
+var PLACE_LAMP = 1;
+var PLACE_COUCH = 2;
+
+var state = LOOK_AROUND
 
 function onLoad() {
     // Setup three.js WebGL renderer. Note: Antialiasing is a big performance hit.
@@ -46,6 +54,8 @@ function onLoad() {
     controls = new THREE.VRControls(camera);
     controls.standing = true;
     camera.position.y = controls.userHeight;
+    camera.position.x = 3;
+    camera.position.z = 3;
 
     // Apply VR stereo rendering to renderer.
     effect = new THREE.VREffect(renderer);
@@ -77,7 +87,10 @@ function onLoad() {
     var testGeometry = new THREE.BoxGeometry(1, 1, 1);
     testObject = new THREE.Mesh(testGeometry, new THREE.MeshPhongMaterial());
     scene.add(testObject);
-    
+    var cursorGeometry = new THREE.BoxGeometry(0.5, 0.5, 0.5);
+    cursor = new THREE.Mesh(cursorGeometry, new THREE.MeshPhongMaterial());
+    scene.add(cursor);
+
     materials.push(new THREE.MeshPhongMaterial());
     materials.push(new THREE.MeshPhongMaterial());
     materials.push(new THREE.MeshPhongMaterial());
@@ -85,14 +98,29 @@ function onLoad() {
 
     var xGeometry = new THREE.BoxGeometry(0.1, 10, 10);
     var zGeometry = new THREE.BoxGeometry(10, 10, 0.1);
-    var wall1 = new THREE.Mesh(xGeometry, materials[1]);
-    var wall2 = new THREE.Mesh(xGeometry, materials[2]);
-    var wall3 = new THREE.Mesh(zGeometry, materials[3]);
-    var wall4 = new THREE.Mesh(zGeometry, materials[4]);
+    var wall1 = new THREE.Mesh(xGeometry, materials[0]);
+    var wall2 = new THREE.Mesh(xGeometry, materials[1]);
+    var wall3 = new THREE.Mesh(zGeometry, materials[2]);
+    var wall4 = new THREE.Mesh(zGeometry, materials[3]);
     wall1.position.x = roomX;
     wall2.position.x = -roomX;
     wall3.position.z = roomZ;
-    wall4.position.z = -roomZ;
+
+    //Adding the 4th wall
+    var onProgress = function ( xhr ) {
+        if ( xhr.lengthComputable ) {
+            var percentComplete = xhr.loaded / xhr.total * 100;
+            console.log( Math.round(percentComplete, 2) + '% downloaded' );
+        }
+    };
+    var onError = function ( xhr ) { };
+    var objLoader = new THREE.OBJLoader();
+    objLoader.setPath( 'models/' );
+    objLoader.load( 'wallWindows.obj', function ( object ) {
+        object.position.z = -roomZ;
+        scene.add( object );
+        walls.push(object);
+    }, onProgress, onError );
 
     scene.add(wall1);
     scene.add(wall2);
@@ -102,11 +130,30 @@ function onLoad() {
     walls.push(wall1);
     walls.push(wall2);
     walls.push(wall3);
-    walls.push(wall4);
 
 
-    var floorGeometry = new THREE.BoxGeometry(1000, 0.1, 1000);
-    floorMaterial = new THREE.MeshPhongMaterial();
+    //Adding some furniture
+    var onProgress = function ( xhr ) {
+        if ( xhr.lengthComputable ) {
+            var percentComplete = xhr.loaded / xhr.total * 100;
+            console.log( Math.round(percentComplete, 2) + '% downloaded' );
+        }
+    };
+    var onError = function ( xhr ) { };
+    var objLoader = new THREE.OBJLoader();
+    objLoader.setPath( 'models/chenilleCouch/' );
+    objLoader.load( 'chenilleCouch.obj', function ( object ) {
+        object.position.x = -4;
+        object.scale.x = 1.5;
+        object.scale.y = 1.5;
+        object.scale.z = 1.5;
+        scene.add( object );
+        couch = object
+    }, onProgress, onError );
+
+    var floorTexture = new THREE.TextureLoader().load( "img/floor.jpg" );
+    var floorGeometry = new THREE.BoxGeometry(10, 0.1, 10);
+    floorMaterial = new THREE.MeshPhongMaterial({map: floorTexture});
     floor = new THREE.Mesh(floorGeometry, floorMaterial);
     scene.add(floor);
 
@@ -122,7 +169,7 @@ function onLoad() {
     vrButton = new webvrui.EnterVRButton(renderer.domElement, uiOptions);
     vrButton.on('exit', function() {
         camera.quaternion.set(0, 0, 0, 1);
-        camera.position.set(0, controls.userHeight, 0);
+        //camera.position.set(0, controls.userHeight, 0);
     });
     vrButton.on('hide', function() {
         document.getElementById('ui').style.display = 'none';
@@ -134,6 +181,9 @@ function onLoad() {
     document.getElementById('magic-window').addEventListener('click', function() {
         vrButton.requestEnterFullscreen();
     });
+
+
+    document.body.addEventListener('click', onClick);
 }
 
 function onTextureLoaded(texture) {
@@ -171,14 +221,25 @@ function floorPositionFromCamera()
     
     if(intersects.length != 0)
     {
-        //console.log(intersects[0].point);
         point = intersects[0].point;
-        testObject.position.set(point.x, point.y, point.z);
-        console.log(testObject.position);
-        console.log(point);
+        //testObject.position.set(point.x, point.y, point.z);
+        return point;
     }
+    return null
 }
 
+function lookingAt(object)
+{
+    raycaster = new THREE.Raycaster(camera.position, camera.getWorldDirection());
+    //raycaster.setFromCamera(new THREE.Vector2(0.5, 0.5))
+
+    var intersects = raycaster.intersectObject(object);
+    if(intersects.length != 0)
+    {
+        return true
+    }
+    return false;
+}
 
 // Request animation frame loop function
 function animate(timestamp) {
@@ -195,7 +256,33 @@ function animate(timestamp) {
     // Render the scene.
     effect.render(scene, camera);
 
-    floorPositionFromCamera();
+    var floorPosition = floorPositionFromCamera();
+
+    if(state == PLACE_LAMP)
+    {
+        if(floorPosition != null)
+        {
+            testObject.position.set(point.x, point.y, point.z);
+        }
+    }
+    if(state == PLACE_COUCH)
+    {
+        if(floorPosition != null)
+        {
+            couch.position.set(point.x, point.y, point.z);
+        }
+    }
+
+
+
+    //Cursor
+    raycaster = new THREE.Raycaster(camera.position, camera.getWorldDirection());
+    var intersects = raycaster.intersectObjects(scene.children);
+    if(intersects.length != 0)
+    {
+        point = intersects[0].point;
+        cursor.position.set(point.x, point.y, point.z);
+    }
 
     vrDisplay.requestAnimationFrame(animate);
 }
@@ -204,6 +291,35 @@ function onResize(e) {
     effect.setSize(window.innerWidth, window.innerHeight);
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
+}
+
+function onClick() {
+    floorPosition = floorPositionFromCamera();
+    console.log(floorPosition);
+
+    console.log(state);
+
+    if(state == LOOK_AROUND)
+    {
+        if(lookingAt(testObject)) {state = PLACE_LAMP};
+        if(lookingAt(couch)) {state = PLACE_COUCH; Console.log("couch")};
+    }
+    else if(state == PLACE_LAMP)
+    {
+        if(floorPosition != null)
+        {
+            testObject.position.set(point.x, point.y, point.z);
+            state = LOOK_AROUND
+        }
+    }
+    else if(state == PLACE_COUCH)
+    {
+        if(floorPosition != null)
+        {
+            couch.position.set(point.x, point.y, point.z);
+            state = LOOK_AROUND
+        }
+    }
 }
 
 // Get the HMD, and if we're dealing with something that specifies
